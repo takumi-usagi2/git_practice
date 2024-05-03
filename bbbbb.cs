bf51f6ec3d406cad87a334f41472fce24879be16
takumi-usagi2 <160295067+takumi-usagi2@users.noreply.github.com>
2024-05-03 22:49:02 +0900

    書いた7

--- bbbbb.cs
+++ bbbbb.cs
@@ -1,13 +1,8 @@
 // aaa
-<<<<<<< HEAD
-public ab = 44444;
 
-public dd = 111;
-=======
 public ab = 43333333;
 
 public cc = 63333333;
 
->>>>>>> c21f5b796c62c37bc42376e16ed88cf57aac6c12
 
 [Serializefield] abstt;
